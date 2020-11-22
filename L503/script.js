f013343ae9718d50d2135101db5c23f56c4b42ba
omanadze <giorgi.omanadze1@gmail.com>
2020-11-23 00:30:18 +0400

    update task 503

--- L503/script.js
+++ L503/script.js
@@ -1,4 +1,4 @@
-let table = document.querySelector('table');
+let table = document.querySelector('#table');
 
 for (let i = 0; i < table.rows.length; i++) {
     table.rows[i].cells[i].style.backgroundColor = 'red';
